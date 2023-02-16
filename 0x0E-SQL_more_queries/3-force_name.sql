a27e7e75838d7794be06aefb4d715c1945e92cbb
IamHendy <puchhendy@gmail.com>
2023-02-16 08:50:38 -0800

    sql queries is fun

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- Write a script that creates the table force_name on your MySQL server
+-- force_name description  
+   -- id INT
+   -- name VARCHAR(256) can’t be null
+
+CREATE TABLE force_name(id INT, name VACHAR(256) NOT NULL);
