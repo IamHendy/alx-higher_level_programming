17e88b4d8a06f1232fbe5eab13081f07f1b86b33
IamHendy <puchhendy@gmail.com>
2023-03-16 02:44:43 +0300

    tasks

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-<<<<<<< HEAD
-exports.add = function (a, b) {
+exports.add = function add (a, b) {
   return a + b;
-=======
-
-exports.add = function add (a, b){
-return a + b;
->>>>>>> c62014feabf2987ef378c15d82e76a66c81be2b1
 };
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,18 +1,3 @@
 #!/usr/bin/node
-<<<<<<< HEAD
-switch (process.argv.length) {
-  case 2:
-    console.log('No argument');
-    break;
-  case 3:
-    console.log('Argument found');
-    break;
-  default:
-    console.log('Arguments found');
-    break;
-}
-=======
-
 const { argv } = require('process');
-if (argv.length === 2) { console.log('No argument'); } else if (argv.length === 3) { console.log('Argument found'); } else { console.log('Arguments found');}
->>>>>>> c62014feabf2987ef378c15d82e76a66c81be2b1
+if (argv.length === 2) { console.log('No argument'); } else if (argv.length === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
